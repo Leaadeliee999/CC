e1c17c6f512fed039e07c6e7d9f0385cf80f6449
Leaaooo <leaadeliee77@gmail.com>
2025-07-17 11:14:14 +0700

    First commit

--- src/pages/Gameplay.jsx
+++ src/pages/Gameplay.jsx
@@ -163,8 +163,6 @@ function Gameplay() {
     });
   };
 
-  console.log("Cek push ulang berhasil");
-
   return (
     <div className="gameplay1" style={{ backgroundImage: `url('/assets/Gameplay.png')` }}>
       <img
